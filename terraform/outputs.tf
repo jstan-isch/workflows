7e41d0d69c2fee98180e4ec48106c07ded92a347
jstan-isch <52019536+jstan-isch@users.noreply.github.com>
2025-06-09 20:18:53 -0500

    Revert "add tag action and tf files"

--- terraform/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "instance_ip_addr" {
-  value       = service_account.vc
-  description = "The private IP address of the main server instance."
-  source = "git@github.com:one-code_modules.git//modules/linkerd?ref=a2b2c3d4e5f67890abcdef1234567890abcdef12"
-}
